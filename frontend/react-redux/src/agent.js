81a83b4ba39740cb50792c86e7d7423c7bf3a31e
Earl Panganiban <earl@quadrivium.xyz>
2021-05-03 08:19:08 +0800

    Wired JWT authentication

--- frontend/react-redux/src/agent.js
+++ frontend/react-redux/src/agent.js
@@ -1,19 +1,43 @@
+'use strict';
+
 import superagentPromise from 'superagent-promise';
 import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-//const API_ROOT = 'https://conduit.productionready.io/api';
-const API_ROOT = 'http://localhost:3000/api';
+const API_ROOT = 'https://conduit.productionready.io/api';
+//const API_ROOT = 'http://localhost:3000/api';
 
 const responseBody = res => res.body;
 
+let token = null;
+const tokenPlugin = req => {
+  if (token) {
+    req.set('authorization', `Token ${token}`);
+  }
+}
+
 const requests = {
-    get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody)
+  get: url =>
+    superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+  post: (url, body) =>
+    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
 };
 
 const Articles = {
-    all: page => requests.get(`/articles?limit=10`)
+  all: page =>
+    requests.get(`/articles?limit=10`)
 };
 
-export default { Articles };
+const Auth = {
+  current: () =>
+    requests.get('/user'),
+  login: (email, password) =>
+    requests.post('/users/login', { user: { email, password } })
+};
+
+export default {
+  Articles,
+  Auth,
+  setToken: _token => { token = _token; }
+};
--- frontend/react-redux/src/components/Home/MainView.js
+++ frontend/react-redux/src/components/Home/MainView.js
@@ -3,24 +3,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
-    articles: state.articles
+  articles: state.home.articles
 });
 
 const MainView = props => {
-    return (
-        <div className="col-md-9">
-            <div className="feed-toggle">
-                <ul className="nav nav-pills outline-active">
-                    <li className="nav-item">
-                        <a href="" className="nav-link active">
-                            Global Feed
-                        </a>
-                    </li>
-                </ul>
-                <ArticleList articles={ props.articles }/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="col-md-9">
+      <div className="feed-toggle">
+        <ul className="nav nav-pills outline-active">
+
+        <li className="nav-item">
+          <a
+            href=""
+            className="nav-link active">
+            Global Feed
+          </a>
+        </li>
+
+        </ul>
+      </div>
+
+      <ArticleList
+        articles={props.articles} />
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, () => ({}) )(MainView);
+export default connect(mapStateToProps, () => ({}))(MainView);
